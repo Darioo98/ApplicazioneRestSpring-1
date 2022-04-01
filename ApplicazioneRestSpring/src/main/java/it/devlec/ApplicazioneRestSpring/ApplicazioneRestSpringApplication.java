f690b5d1471ee182c57245a27419a1171d0e7852
StefanoDeDonatis <dedonatisstefano02@gmail.com>
2022-04-01 11:19:56 +0200

    Aggiunta del main spring

---
+++ ApplicazioneRestSpring/src/main/java/it/devlec/ApplicazioneRestSpring/ApplicazioneRestSpringApplication.java
@@ -0,0 +1,13 @@
+package it.devlec.ApplicazioneRestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneRestSpringApplication.class, args);
+	}
+
+}
